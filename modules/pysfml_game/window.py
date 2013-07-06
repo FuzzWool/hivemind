0cb42a0b93b99a4e758492402969e94c8c16356b
Sam <message.me.sam@gmail.com>
2013-07-06 19:53:41 +0100

    0.0.1 - Added Level loading!
    + Added Level class.
    Level loads level data from a txt file.
    
    Changed Window. Window is now, by default, a double-scaled view.

--- modules/pysfml_game/window.py
+++ modules/pysfml_game/window.py
@@ -1,6 +1,15 @@
 import sfml as sf
 
-SCREEN_WIDTH = 1024; SCREEN_HEIGHT = 768
+SCALE = 2
+
+SCREEN_WIDTH = 400 * SCALE
+SCREEN_HEIGHT = 300 * SCALE
 window = sf.RenderWindow(sf.VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Hivemind")
 window.framerate_limit = 60
 window.vertical_sync_enabled = True
+
+view = sf.View.from_rect(\
+	sf.FloatRect(0, 0, SCREEN_WIDTH/SCALE, SCREEN_HEIGHT/SCALE))
+window.view = view
+
+GRID = 25
--- test1.py
+++ test1.py
@@ -1,19 +1,61 @@
 import modules as mo
 rtrn = mo.KeyTracker(mo.sf.Keyboard.RETURN)
 
-tex = mo.sf.Texture.load_from_file("img/test.png")
-sprites = []
-def make_sprite():
-	i = len(sprites)
-	sprite = mo.MySprite(tex)
-	sprite.clip.set(25, 25)
-	sprite.clip.use(i, 0)
-	sprite.position = 100 + (i * 50), 100
-	sprites.append(sprite)
+class Level:
+	tiles = []
+	def __init__ (self):
 
-for i in range(5):
-	make_sprite()
+		def get_level():
+		#Grab level data from text file.
+			f = open("outside/level.txt")
+			level = f.read()
+			f.close()
+			return level
 
+		def format_level(level):
+		#The level is now in an [x][y] format.
+			rows = level.split("\n")
+			format = []
+			i = 0
+			while i < len(rows[0])-1:
+				format.append([c[i]+c[i+1] for c in rows])
+				i += 2
+			return format
+
+		def make_tile(x, y):
+		#Make a tile in a specific position.
+			tex = mo.sf.Texture.load_from_file\
+				("img/test/level.png")
+			sprite = mo.MySprite(tex)
+			sprite.clip.set(mo.GRID, mo.GRID)
+			sprite.clip.use(x, y)
+			return sprite
+
+		def make_tiles(level):
+		#Add tile graphics based on the level[x][y]
+			#Make the tiles.
+			alphabet = ["a", "b", "c", "d", "e", "f", "g",\
+			"h", "i", "j", "k", "l", "m", "n", "o", "p", "q",\
+			"r", "s", "t", "u", "v", "w", "x", "y", "z"]
+			for x_i, x in enumerate(level):
+				for y_i, y in enumerate(x):
+					tile_x = alphabet.index(y[0])
+					tile_y = alphabet.index(y[1])
+					tile = make_tile(tile_x, tile_y)
+					tile.goto = mo.GRID*x_i, mo.GRID*y_i
+					tile.move(x_i*10, y_i)
+					self.tiles.append(tile)
+
+		level = get_level()
+		level = format_level(level)
+		make_tiles(level)
+
+	def draw(self):
+		for s in self.tiles:
+			s.draw()
+
+
+Level = Level()
 #########################################################
 running = True
 while running:
@@ -29,7 +71,6 @@ while running:
 	#Video
 	mo.window.clear(mo.sf.Color.WHITE)
 	#
-	for s in sprites:
-		s.draw()
+	Level.draw()
 	#
 	mo.window.display()
