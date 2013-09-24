04d167d10521ef7166f1b55e7af2da942d763e09
FuzzWool <sam.tom.drake@gmail.com>
2013-09-24 19:56:53 +0100

    + Access TILES globally from WORLDMAP.
    + Access TILES globally from WORLDMAP.
    Uses:
    * Collision detection batching. (Multi-slope checks)
    * Drawing only ON-SCREEN tiles from WorldMap.

--- room_test.py
+++ room_test.py
@@ -26,7 +26,8 @@ class Room(object):
 
 	def load(self, room_x=0, room_y=0):
 		#LOAD and POSITION the tiles.
-		#Consider the ROOM OFFSET, too!
+		#Consider the ROOM OFFSET.
+
 		tiles = []
 		w, h = RENDER_WIDTH/GRID, RENDER_HEIGHT/GRID
 		room_x *= RENDER_WIDTH/GRID
@@ -77,10 +78,69 @@ class Room(object):
 
 ####
 
+class WorldMap:
+#Loads MULTIPLE ROOMS.
+#Provides shorthands for accessing all the rooms as one.
+	
+	def __init__(self):
+	#Load all of the ROOMS.
+		self.rooms = self.load(2,2)
+
+
+	# TILE DRAWING
+
+	def load(self, w=0, h=0):
+	#Specify the AMOUNT of rooms to be loaded.
+		rooms = []
+		for x in range(w):
+			rooms.append([])
+			for y in range(h):
+				room = Room(x,y)
+				rooms[-1].append(room)
+		return rooms
+
+	def draw(self, camera=None):
+	#Draws all of the ROOMS in the game.
+	#! Draw only ROOMS shown by a Camera.
+
+		for x in self.rooms:
+			for y in x:
+				y.draw()
 
 
 
-room = Room(0,0)
+	# TILE ACCESS
+
+	@property
+	def tiles(self):
+	#An xy-list of the tiles of every single room.
+		
+		tiles = []
+		#Grab every ROOM in a COLUMN.
+		for room_x in self.rooms:
+
+			#For every TILE COLUMN...
+			tile_w = len(room_x[0].tiles)
+			for tile_x in range(tile_w):
+				#...grab it from all the ROOMS,
+				# and connect them.
+				column = []
+				for room_y in room_x:
+					column += room_y.tiles[tile_x]
+				tiles.append(column)
+
+		return tiles
+
+	@property
+	def tiles_w(self): return len(self.tiles)
+	@property
+	def tiles_h(self): return len(self.tiles[0])
+
+####
+
+
+worldmap = WorldMap()
+
 
 #########################################################
 
@@ -92,10 +152,20 @@ while running:
 	if key.RETURN.pressed():
 		pass
 
+	amt = 5
+	if key.LEFT.held(): Camera.x -= amt
+	if key.RIGHT.held(): Camera.x += amt
+	if key.UP.held(): Camera.y -= amt
+	if key.DOWN.held(): Camera.y += amt
+
 	#VIDEO
 	window.clear(sf.Color.WHITE)
 	window.view = Camera
 	
-	room.draw()
+
+	#
+	worldmap.draw(10)
+	#
+
 
 	window.display()
